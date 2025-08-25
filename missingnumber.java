25833452ea5e0016cf24f233ee5f69ed766746b0
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2025-08-25 17:23:15 +0600

    day-10 problem solved

---
+++ missingnumber.java
@@ -0,0 +1,27 @@
+public class missingnumber {
+
+    //day-10
+    public static int missingNumber() {
+
+        int[] nums={7,6,4,2,3,5,8,0,1};
+
+        int num=nums.length;
+        int flag=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i==nums[j]){
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                num=i;
+                break;  
+            }
+            flag=0;
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        System.out.println(missingNumber());
+    }
+}
