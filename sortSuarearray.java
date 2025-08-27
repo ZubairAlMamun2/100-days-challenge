bc57176987669b186e57d98b40197648668c3a1f
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2025-08-27 19:38:14 +0600

    day-12 problem solved

---
+++ sortSuarearray.java
@@ -0,0 +1,29 @@
+public class sortSuarearray {
+
+    //day-12
+ public int[] sortedSquares(int[] nums) {
+                
+         for(int j=0;j<nums.length;j++){
+            if(j==0){
+                nums[j]=nums[j]*nums[j];
+            }
+            for(int i=0;i<nums.length-1;i++){
+            if(j==0){
+                nums[i+1]=nums[i+1]*nums[i+1];
+            }
+            if(nums[i]>nums[i+1]){
+                nums[i]=nums[i]+nums[i+1];
+                nums[i+1]=nums[i]-nums[i+1];
+                nums[i]=nums[i]-nums[i+1];
+
+            }
+        }
+        }
+        
+        return nums;
+    }
+    public static void main(String[] args) {
+        int nums[] = {-4,-1,0,3,10};
+        sortedSquares(nums);
+    }
+}
